49b125175852a794604c719791a74bb83efcfb23
AlfianPostman <kinobabes@gmail.com>
2026-03-02 13:05:35 +0700

    [fix] homepage update

---
+++ src/layouts/Kuntul.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+type Country = {
+  name: string;
+  code: string;
+};
+
+defineProps<{
+    data: Record<string, Country[]>;
+}>();
+
+</script>
+
+<template>
+    <p>ADDDSSSDDASD {{ data }}</p>
+</template>
