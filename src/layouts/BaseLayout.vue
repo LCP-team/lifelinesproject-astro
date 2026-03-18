afbb67333a460ffe4f3ce40264a6ed5f07641b69
Ifan Fairuz <ifanfairuz@gmail.com>
2026-03-18 22:16:19 +0000

    fix tittle

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -9,20 +9,20 @@
 </template>
 
 <script setup lang="ts">
-import { watchEffect, useSSRContext } from 'vue'
-import Navbar from '../components/Navbar.vue'
-import Footer from '../components/Footer.vue'
+import { onMounted, useSSRContext } from "vue";
+import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
 
 const props = defineProps<{
-  title?: string
-}>()
+  title?: string;
+}>();
 
 if (import.meta.env.SSR) {
-  const ctx = useSSRContext<{ title?: string }>()
-  if (ctx && props.title) ctx.title = props.title
+  const ctx = useSSRContext<{ title?: string }>();
+  if (ctx && props.title) ctx.title = `${props.title} | LifeLines Canada`;
 } else {
-  watchEffect(() => {
-    if (props.title) document.title = props.title
-  })
+  onMounted(() => {
+    if (props.title) document.title = props.title;
+  });
 }
 </script>
