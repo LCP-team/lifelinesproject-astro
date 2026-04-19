c558230e87e36475ef7fd4e0baa5cb35f4b7f0fc
Ifan Fairuz <ifanfairuz@gmail.com>
2026-04-19 22:38:33 +0700

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL?.trim() ||
-  `https://api.${window.location.hostname}`;
+  import.meta.env.VITE_API_BASE_URL?.trim() || `https://api.sophiaathena.com`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
